755d35ee1cd5af9304f6423b79821f1cf382eb10
bluesky2008 <william_billl2008@yahoo.co.jp>
2022-03-08 13:51:40 +0900

    ヘッダー・フッター実装

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>GOLJO</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=yes" />
+
+        <link rel="icon" type="image/png" href="{{ asset('img/omisenavi_favicon.png') }}">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <!-- <link rel="icon" type="image/png" href="https://mall.cpon.co.jp/favicon.png"> -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+
+    <div id="registration_pc">
+        <body>
+            <header>
+                <a class="logo_a" href="/">
+                    <img src="{{ asset('img/logo.png') }}" class="header_logo" alt="">
+                </a>
+                <div class="header_menu">
+                    <a href="" class="header_a">GOLJOサーチ</a>
+                    <a href="" class="header_a">Pickup GOLJO</a>
+                    <a href="" class="header_a">イベント</a>
+                    <a href="" class="header_a">オススメGOLF場</a>
+                    <a href="" class="header_a">練習場</a>
+                    <a href="" class="header_a">ニュース</a>
+                    <a href="" class="header_a">GOLJO 人気ランキング</a>
+                    <a href="" class="header_a">Fashionカタログ</a>
+                </div>
+            </header>
+
+            @yield('content')
+
+            <footer>
+                <a href="/"><img src="{{ asset('img/logo2.png') }}" class="footer_logo" alt=""></a>
+                <div class="footer_menu">
+                    <a href="" class="footer_a">GOLJOサーチ</a> 　｜　
+                    <a href="" class="footer_a">Pickup GOLJO</a> 　｜　
+                    <a href="" class="footer_a">イベント</a> 　｜　
+                    <a href="" class="footer_a">オススメGOLF場</a> 　｜　
+                    <a href="" class="footer_a">練習場</a> 　｜　
+                    <a href="" class="footer_a">ニュース</a> 　｜　
+                    <a href="" class="footer_a">GOLJO 人気ランキング</a> 　｜　
+                    <a href="" class="footer_a">Fashionカタログ</a>
+                </div>
+                <div class="copyright">2022 ©︎ GOLJO All right.</div>
+            </footer>
+        </body>
+    </div>
+
+    <div id="registration_sp">
+        <body>
+            <header class="mb-4">
+                @yield('back_button')
+                <div class="cpon_logo_div_sp">
+                    <a href="/">
+                        <img src="{{ asset('img/logo.png') }}" class="cpon_logo_sp" alt="">
+                    </a>
+                </div>
+            </header>
+
+            <div class="body_sp">
+            @yield('content_sp')
+            </div>
+
+            <footer class="footer_sp">
+                <a href="#page_top">ページTOPへ</a>
+                <div class="copyright_sp">2022 ©︎ GOLJO All right.</div>
+            </footer>
+        </body>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript">
+    if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) 
+        || navigator.userAgent.indexOf('iPod') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+        document.getElementById('registration_pc').style.display = "none";
+        document.getElementById('registration_sp').style.visibility = "visible";
+    } else {
+        document.getElementById('registration_sp').style.display = "none";
+    }
+    </script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'App\Http\Controllers\GoljoController@index')->name('index');
+Route::post('app_store', 'App\Http\Controllers\GoljoController@app_store')->name('app_store');
+Route::get('settle', 'App\Http\Controllers\GoljoController@settle')->name('settle');
+Route::post('confirm_post', 'App\Http\Controllers\GoljoController@confirm_post')->name('confirm_post');
+Route::get('confirm', 'App\Http\Controllers\GoljoController@confirm')->name('confirm');
+Route::post('txid_store', 'App\Http\Controllers\GoljoController@txid_store')->name('txid_store');
+Route::get('complete', 'App\Http\Controllers\GoljoController@complete')->name('complete');
+Route::get('dashboard', 'App\Http\Controllers\GoljoController@dashboard')->name('dashboard')->middleware('login_user');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('admin/app_list', 'App\Http\Controllers\GoljoController@app_list')->name('admin.app_list')->middleware('login_admin');
+Route::get('admin/app_list_delete', 'App\Http\Controllers\GoljoController@app_list_delete')->name('admin.app_list_delete')->middleware('login_admin');
+Route::get('admin/search', 'App\Http\Controllers\GoljoController@search')->name('admin.search')->middleware('login_admin');
+
+// ユーザーログイン
+Route::get('login', function () { return view('login_user'); });
+Route::get('reminder', function () { return view('reminder'); });
+
+Route::POST('/user_login', 'App\Http\Controllers\GoljoController@login_user')->name('login');
+Route::get('/user_logout', 'App\Http\Controllers\GoljoController@logout_user')->name('logout')->middleware('login_user');
+
+// 管理側ログイン
+Route::get('admin/login', function () { return view('login_admin'); });
+
+Route::POST('/admin_login', 'App\Http\Controllers\GoljoController@login_admin')->name('admin.login');
+Route::get('/admin_logout', 'App\Http\Controllers\GoljoController@logout_admin')->name('admin.logout')->middleware('login_admin');
